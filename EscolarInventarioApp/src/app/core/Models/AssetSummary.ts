9eaaee7dbacfc9869a4c4fe12f54f35b808a1aff
Fernando Cesar <juniorlemosoi@gmail.com>
2025-11-16 00:09:39 -0300

    Remove duplicated Models folder (uppercase)

--- EscolarInventarioApp/src/app/core/Models/AssetSummary.ts
+++
@@ -1,8 +0,0 @@
-export interface AssetSummary {
-  id: number;
-  name: string;
-  roomLocation?: {
-    id: number;
-    name: string;
-  };
-}
--- EscolarInventarioApp/src/app/core/Models/enum/ConservationState.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum ConservationState {
-  IRRECUPERAVEL = 1,
-  DANIFICADO = 2,
-  REGULAR = 3,
-  BOM = 4,
-  NOVO = 5
-}
