c634768f421b0efa1f64ab3db1355df92d3157c9
Fernando Cesar <juniorlemosoi@gmail.com>
2025-11-25 11:55:52 -0300

    Correção de string de consulta

--- src/Backend/InventarioEscolar.Infrastructure/DataAccess/Repositories/SchoolRepository.cs
+++ src/Backend/InventarioEscolar.Infrastructure/DataAccess/Repositories/SchoolRepository.cs
@@ -73,10 +73,10 @@ namespace InventarioEscolar.Infrastructure.DataAccess.Repositories
         public async Task<School?> GetDuplicateSchool(string name, string? inep, string? address)
         {
             return await dbContext.Schools
-            .FirstOrDefaultAsync(s =>
-             EF.Functions.Collate(s.Name, "Latin1_General_CI_AS") == name
-             || (inep != null && s.Inep == inep)
-             || (address != null && s.Address == address));
+                .FirstOrDefaultAsync(s =>
+                    s.Name.ToLower() == name.ToLower() ||
+                    (inep != null && s.Inep == inep) ||
+                    (address != null && s.Address == address));
         }
         public async Task Insert(School school) => await dbContext.Schools.AddAsync(school);
 
