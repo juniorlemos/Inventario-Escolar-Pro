040808954e80b1527ceb830a21a104bcc4bd704d
Fernando Cesar <juniorlemosoi@gmail.com>
2025-12-12 23:25:01 -0300

    Ajuste em arquivos de segurança

--- src/Backend/InventarioEscolar.Application/UsesCases/AuthService/ForgotPassword/ForgotPasswordCommandHandler.cs
+++ src/Backend/InventarioEscolar.Application/UsesCases/AuthService/ForgotPassword/ForgotPasswordCommandHandler.cs
@@ -4,11 +4,13 @@ using InventarioEscolar.Exceptions;
 using InventarioEscolar.Exceptions.ExceptionsBase;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 
 namespace InventarioEscolar.Application.UsesCases.AuthService.ForgotPassword
 {
     public class ForgotPasswordCommandHandler(
         UserManager<ApplicationUser> userManager,
+        IConfiguration configuration,
         IEmailService emailService) : IRequestHandler<ForgotPasswordCommand,Unit>
     {
         public async Task<Unit> Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
@@ -17,7 +19,11 @@ namespace InventarioEscolar.Application.UsesCases.AuthService.ForgotPassword
                 ?? throw new NotFoundException(ResourceMessagesException.E_MAIL_NOT_FOUND);
             
             var token = await userManager.GeneratePasswordResetTokenAsync(user);
-            var resetLink = $"http://localhost:4200/reset-password?token={Uri.EscapeDataString(token)}&email={user.Email}";
+
+            var frontendUrl = configuration["FRONTEND_URL"]
+            ?? throw new BusinessException("FRONTEND_URL não configurada");
+
+            var resetLink = $"{frontendUrl}/reset-password?token={Uri.EscapeDataString(token)}&email={Uri.EscapeDataString(user.Email!)}";
 
             var message = $"<p>Clique no link abaixo para redefinir sua senha:</p><p><a href='{resetLink}'>Redefinir senha</a></p>";
             await emailService.SendEmailAsync(user.Email!, "Redefinir Senha", message);
